82b78bd167a9cdc0fd4c5c01394010b99aa5f103
Anna Palaszkiewicz <a.m.palaszkiewicz@gmail.com>
2020-05-21 19:58:28 +0200

    start/header

--- src/components/Home/Start/Start.js
+++ src/components/Home/Start/Start.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import {NavLink as LinkRouter} from "react-router-dom";
+ import decoration from "../../../assets/Decoration.svg"
 
 export default function Start() {
     return (
@@ -6,7 +8,15 @@ export default function Start() {
         <div className="banner" id='start'>
         </div>
             <div className="start-help">
-                Zacznij pomagać- oddaj niechaciane rzeczy
+                <h1>Zacznij pomagać!<br></br>
+                    Oddaj niechaciane rzeczy w zaufane ręce</h1>
+                <img src={decoration} alt=""/>
+                <div className="button">
+                    <ul>
+                        <li><LinkRouter className="navlink" to="/login" >Oddaj rzeczy</LinkRouter></li>
+                        <li><LinkRouter className="navlink" to="/login" >Zorganizuj zbiórkę</LinkRouter></li>
+                    </ul>
+                </div>
             </div>
             </div>
     );
