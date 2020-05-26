27686941bc2d3de26501b33174b0210c2741ce18
Anna Palaszkiewicz <a.m.palaszkiewicz@gmail.com>
2020-05-26 21:10:39 +0200

    pagination

--- src/components/Home/OurHelp/OurHelp.js
+++ src/components/Home/OurHelp/OurHelp.js
@@ -7,11 +7,18 @@ let organizations = data.fundations[1];
 let local = data.fundations[2];
 
 export default function OurHelp() {
-    const [objectToShow, setObjectToShow]=useState(fundations);
+    const [objectToShow, setObjectToShow] = useState(fundations);
     const [currentList, setCurrentList] = useState([]);
     const [currentPage, setCurrentPage] = useState(1);
     const [dataPerPage, setDataPerPage] = useState(3);
-
+    const [active, setActive] = useState({
+        fundations: true,
+        organizations: false,
+        local: false
+    });
+    const activeStyle = {
+        border: "0.75px solid gray"
+    };
 
     const NumbersOfPages = (list) => {
         const pageNumbers = [];
@@ -21,38 +28,77 @@ export default function OurHelp() {
         return pageNumbers;
     };
     const handleClick = e => {
-        setCurrentPage(e.target.id)
+        setCurrentPage(e.target.id);
     };
 
     useEffect(() => {
         setCurrentList(objectToShow.items.slice(dataPerPage * (currentPage - 1), dataPerPage * currentPage))
-    }, [currentPage]);
+    }, [currentPage, objectToShow]);
 
     return (
-        <div className="start" id='ourHelp'>
+        <div className="start" id="ourHelp">
             <div>
                 <h1>Komu pomagamy?</h1>
                 <img className="decoration" src={decoration} alt=""/>
             </div>
-            <div>
-                <button onClick={() => setObjectToShow(fundations)}>Fundacjom</button>
-                <button onClick={() => setObjectToShow(organizations)}>Organizacjom pozarządowym</button>
-                <button onClick={() => setObjectToShow(local)}>Lokalnym zbiórkom</button>
+            <div className="button btns">
+                <button style={active.fundations ? activeStyle:{}} className="navlink" onClick={() => {
+                    setObjectToShow(fundations);
+                    setCurrentPage(1);
+                    setActive({
+                        fundations: true,
+                        organizations: false,
+                        local: false
+                    })
+                }}>Fundacjom
+                </button>
+                <button style={active.organizations ? activeStyle:{}} className="navlink" onClick={() => {
+                    setObjectToShow(organizations);
+                    setCurrentPage(1);
+                    setActive({
+                        fundations: false,
+                        organizations: true,
+                        local: false
+                    })
+                }}>Organizacjom pozarządowym
+                </button>
+                <button style={active.local ? activeStyle:{}} className="navlink" onClick={() => {
+                    setObjectToShow(local);
+                    setCurrentPage(1);
+                    setActive({
+                        fundations: false,
+                        organizations: false,
+                        local: true
+                    })
+                }}>Lokalnym zbiórkom
+                </button>
 
             </div>
-            <h1>{objectToShow.desc}</h1>
-            <div>
+            <h2>{objectToShow.desc}</h2>
+            <div className="helpFor">
                 <ul>
-                    {currentList.map((el, ind) => <li key={ind}><h1>{el.header}</h1><h2>{el.subheader}</h2><h3>{el.desc}</h3></li>)}
+                    {currentList.map((el, ind) =>
+                        <li key={ind}>
+                            <div><h1>{el.header}</h1><h3>{el.subheader}</h3></div>
+                            <h3>{el.desc}</h3></li>)}
                 </ul>
-                <ul id="page-numbers">
-                    {NumbersOfPages(objectToShow.items).map(number => {
-                        return (
-                            <li key={number} id={number} onClick={handleClick}>
-                                {number}
-                            </li>
-                        );
-                    })}
+                <ul id="page-numbers">{objectToShow.items.length >3 ?
+                    NumbersOfPages(objectToShow.items).map((number, ind) => {
+                        if (ind === currentPage - 1) {
+                            return (
+                                <li style={{border: "0.75px solid gray"}} key={number} id={number}
+                                    onClick={handleClick}>
+                                    {number}
+                                </li>
+                            );
+                        } else {
+                            return (
+                                <li key={number} id={number} onClick={handleClick}>
+                                    {number}
+                                </li>
+                            )
+                        }
+                    }):<li></li>}
                 </ul>
             </div>
         </div>
--- src/components/Home/OurHelp/data.js
+++ src/components/Home/OurHelp/data.js
@@ -1,114 +1,115 @@
 let data = {
-    "fundations":[
-    {
-        "name":"Fundacjom",
-        "desc":"jakis opis fundacje",
-        "items":[
-            {
-                "header":"to jest header",
-                "subheader":"to jest subheader",
-                "desc":"to jest desc"
-            },
-            {
-                "header":"to jest header",
-                "subheader":"to jest subheader",
-                "desc":"to jest desc"
-            },
-            {
-                "header":"to jest header",
-                "subheader":"to jest subheader",
-                "desc":"to jest desc"
-            },
-            {
-                "header":"to jest header",
-                "subheader":"to jest subheader",
-                "desc":"to jest desc"
-            },
-            {
-                "header":"to jest header",
-                "subheader":"to jest subheader",
-                "desc":"to jest desc"
-            },
-            {
-                "header":"to jest header",
-                "subheader":"to jest subheader",
-                "desc":"to jest desc"
-            },
-            {
-                "header":"to jest header",
-                "subheader":"to jest subheader",
-                "desc":"to jest desc"
-            },
-            {
-                "header":"to jest header",
-                "subheader":"to jest subheader",
-                "desc":"to jest desc"
-            },
-            {
-                "header":"to jest header",
-                "subheader":"to jest subheader",
-                "desc":"to jest desc"
-            }
-        ]
-    },
-    {
-        "name":"Ogganizacjom",
-        "desc":"jakis opis organizacje",
-        "items":[
-            {
-                "header":"to jest header",
-                "subheader":"to jest subheader",
-                "desc":"to jest desc"
-            },
-            {
-                "header":"to jest header",
-                "subheader":"to jest subheader",
-                "desc":"to jest desc"
-            },
-            {
-                "header":"to jest header",
-                "subheader":"to jest subheader",
-                "desc":"to jest desc"
-            },
-            {
-                "header":"to jest header",
-                "subheader":"to jest subheader",
-                "desc":"to jest desc"
-            },
-            {
-                "header":"to jest header",
-                "subheader":"to jest subheader",
-                "desc":"to jest desc"
-            },
-            {
-                "header":"to jest header",
-                "subheader":"to jest subheader",
-                "desc":"to jest desc"
-            }
-        ]
-    },
-    {
-        "name":"Local",
-        "desc":"jakis opis lokalsi",
-        "items":[
-            {
-                "header":"to jest header",
-                "subheader":"to jest subheader",
-                "desc":"to jest desc"
-            },
-            {
-                "header":"to jest header",
-                "subheader":"to jest subheader",
-                "desc":"to jest desc"
-            },
-            {
-                "header":"to jest header",
-                "subheader":"to jest subheader",
-                "desc":"to jest desc"
-            }
-        ]
-    }
-]
+    "fundations": [
+        {
+            "name": "Fundacjom",
+            "desc": "W naszej bazie znajdziesz listę zweryfikowanych Fundacji, z którymi współpracujemy. Możesz sprawdzić czym się zajmują, komu pomagają i czego potrzebują. ",
+            "items": [
+                {
+                    "header": " Fundacja 1",
+                    "subheader": "Cel i misja: pomoc osobom znajdującym się w trudnej sytuacji życiowej.",
+                    "desc": "ubrania, jedzenie, sprzęt AGD, meble, zabawki"
+
+                },
+                {
+                    "header": "Fundacja 2",
+                    "subheader": "Cel i misja: pomoc dzieciom",
+                    "desc": "ubrania, zabawki"
+                },
+                {
+                    "header": "Fundacja 3",
+                    "subheader": "Cel i misja: Pomoc osobom bezdomnym",
+                    "desc": "ubrania, jedzenie, koce"
+                },
+                {
+                    "header": "Fundacja 4",
+                    "subheader": "Cel i misja: ......",
+                    "desc": "jedzenie, ubrania, meble, środki czystosci"
+                },
+                {
+                    "header": "Fundacja 5",
+                    "subheader": "Cel i misja: ......",
+                    "desc": "jedzenie"
+                },
+                {
+                    "header": "Fundacja 6",
+                    "subheader": "Cel i misja: ......",
+                    "desc": "ubrania"
+                },
+                {
+                    "header": "Fundacja 7",
+                    "subheader": "Cel i misja: ......",
+                    "desc": "środki czystości"
+                },
+                {
+                    "header": "Fundacja 8",
+                    "subheader": "Cel i misja: ......",
+                    "desc": "jedzenie"
+                },
+                {
+                    "header": " Fundacja 9",
+                    "subheader": "Cel i misja: ......",
+                    "desc": "jedzenie"
+                }
+            ]
+        },
+        {
+            "name": "Organizacjom",
+            "desc": "Takim organizacjom pomagamy",
+            "items": [
+                {
+                    "header": "Organizacja 1",
+                    "subheader": "cel i misja:...",
+                    "desc": "ubrania"
+                },
+                {
+                    "header": "Organizacja 2",
+                    "subheader": "cel i misja:...",
+                    "desc": "datki"
+                },
+                {
+                    "header": "Organizacja 3",
+                    "subheader": "cel i misja:...",
+                    "desc": "meble"
+                },
+                {
+                    "header": "Organizacja 4",
+                    "subheader": "cel i misja:...",
+                    "desc": "czas"
+                },
+                {
+                    "header": "Organizacja 5",
+                    "subheader": "cel i misja:...",
+                    "desc": "środki"
+                },
+                {
+                    "header": "Organizacja 6",
+                    "subheader": "cel i misja:...",
+                    "desc": "jedzenie"
+                }
+            ]
+        },
+        {
+            "name": "Local",
+            "desc": "Pomagamy lokalnym zbiórkom...",
+            "items": [
+                {
+                    "header": "Zbiórka 1",
+                    "subheader": "cel i misja: ......",
+                    "desc": "jedzenie"
+                },
+                {
+                    "header": "Zbiórka 2",
+                    "subheader": "cel i misja: ......",
+                    "desc": "zabawki"
+                },
+                {
+                    "header": "Zbiórka 3",
+                    "subheader": "cel i misja: ......",
+                    "desc": "środki czystości"
+                }
+            ]
+        }
+    ]
 };
 
 export default data;
