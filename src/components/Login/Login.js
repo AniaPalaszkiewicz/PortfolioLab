673fb75f7be5e43f4ab13497a89d23757d742d8b
Anna Palaszkiewicz <a.m.palaszkiewicz@gmail.com>
2020-05-28 22:16:33 +0200

    validation

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,21 +1,64 @@
-import React from 'react';
+import React, {useState} from 'react';
 import decoration from "../../assets/Decoration.svg";
 import {NavLink as LinkRouter} from "react-router-dom";
 
 export default function Login() {
+    const [errors, setErrors] = useState({
+        email: "",
+        password: ""
+    });
+    const [login, setLogin] = useState({
+        email: "",
+        password: ""
+    });
+    const handleChange = e => {
+        e.preventDefault();
+        setErrors({
+            email: "",
+            password: ""
+        });
+        setLogin({...login, [e.target.name]: e.target.value})
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        let email = "";
+        let password = "";
+        const reg = /^([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22))*\x40([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d))*(\.\w{2,})+$/;
+        if(!login.email.match(reg)){
+            email= "Podany email jest nieprawidłowy"
+        }
+        if(login.password.length < 6){
+            password="Podane hasło jest za krótkie"
+        }
+        if(email || password){
+            setErrors({...errors,email:email, password:password});
+            return;
+        }
+        setLogin({
+            email:"",
+            password:""
+        });
+        setErrors({
+            email:"",
+            password:""
+        });
+    };
     return (
         <div className="login">
             <h1>Zaloguj się</h1>
             <img className="decoration" src={decoration} alt=""/>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <div className="login-form">
                     <label>
                         Email
-                        <input type="email"/>
+                        <input onChange={handleChange} name="email" type="email" value={login.email}/>
+                        {errors.email && <p>{errors.email}</p>}
                     </label>
                     <label>
                         Hasło
-                        <input type="password"/>
+                        <input onChange={handleChange} type="password" name="password" value={login.password}/>
+                        {errors.password && <p>{errors.password}</p>}
                     </label>
                 </div>
                 <div className="button two">
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,25 +1,78 @@
-import React from 'react';
+import React,{useState} from 'react';
 import decoration from "../../assets/Decoration.svg";
 import {NavLink as LinkRouter} from "react-router-dom";
 
 export default function Register() {
+    const [errors, setErrors] = useState({
+        email: "",
+        password: "",
+        password2:""
+    });
+    const [register, setRegister] = useState({
+        email: "",
+        password: "",
+        password2:""
+    });
+    const handleChange = e => {
+        e.preventDefault();
+        setErrors({
+            email: "",
+            password: "",
+            password2:""
+        });
+        setRegister({...register, [e.target.name]: e.target.value})
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        let email = "";
+        let password = "";
+        let password2="";
+        const reg = /^([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22))*\x40([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d))*(\.\w{2,})+$/;
+        if(!register.email.match(reg)){
+            email= "Podany email jest nieprawidłowy"
+        }
+        if(register.password.length < 6 || !register.password){
+            password="Podane hasło jest za krótkie"
+        }
+        if((register.password2.length < 6 && register.password !== register.password2) || !register.password2){
+            password2="Podane hasła są różne"
+        }
+        if(email || password || password2){
+            setErrors({...errors,email:email, password:password, password2:password2});
+            return;
+        }
+        setRegister({
+            email:"",
+            password:"",
+            password2:""
+        });
+        setErrors({
+            email:"",
+            password:"",
+            password2:""
+        });
+    };
     return (
         <div className="login">
             <h1>Załóż konto</h1>
             <img className="decoration" src={decoration} alt=""/>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <div className="login-form register">
                     <label>
                         Email
-                        <input type="email"/>
+                        <input onChange={handleChange} name="email" type="email" value={register.email}/>
+                        {errors.email && <p>{errors.email}</p>}
                     </label>
                     <label>
                         Hasło
-                        <input type="password"/>
+                        <input onChange={handleChange} type="password" name="password" value={register.password}/>
+                        {errors.password && <p>{errors.password}</p>}
                     </label>
                     <label>
                         Powtórz hasło
-                        <input type="password"/>
+                        <input onChange={handleChange} type="password" name="password2" value={register.password2}/>
+                        {errors.password2 && <p>{errors.password2}</p>}
                     </label>
                 </div>
                 <div className="button two">
