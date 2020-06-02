434742fdad7a60c6c33949398fdbf463f1c86eef
Anna Palaszkiewicz <a.m.palaszkiewicz@gmail.com>
2020-06-02 21:41:55 +0200

    form

--- src/components/GiveThings/ThingsToGive/Step4.js
+++ src/components/GiveThings/ThingsToGive/Step4.js
@@ -1,7 +1,47 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 export default function Step4({props}) {
-    const{counter, handleChange, formData, Minus, Plus}=props;
+    const [errors, setErrors] = useState({
+        street: "",
+        city: "",
+        code: "",
+        phone: "",
+        date: "",
+        hour: ""
+
+    });
+
+    const {counter, handleChange, formData, Minus, Plus} = props;
+
+    const four = () => {
+        let street = '';
+        let city = "";
+        let code = "";
+        let phone = "";
+        let date = "";
+        let hour = "";
+        if (formData.street.length < 2) {
+            street = "Minimum 2 znaki!"
+        }
+        if (formData.city < 2) {
+            city = "Minimum 2 znaki!"
+        }
+        if(formData.code.length !== 5){
+            code="Kod musi składać się z 5 cyfr!!"
+        }
+        if(formData.phone.length !== 9){
+            phone="Numer musi składać się z 9 cyfr!"
+        }
+        if(!formData.date || !formData.hour){
+            date="Proszę uzupełnić to pole"
+        }
+        if (street || city || code || phone || date || hour) {
+            setErrors({...errors, street: street, city: city, code: code, phone: phone, date : date, hour : date });
+            return;
+        }
+        Plus();
+    };
+
     return (
         <div className="step1">
             <h5>Krok {counter} /4</h5>
@@ -9,20 +49,26 @@ export default function Step4({props}) {
             <form className="form-step4">
                 <div>
                     <h3>Adres odbioru:</h3>
-                    <label> Ulica <input type="text"/> </label>
-                    <label> Miasto <input type="text"/> </label>
-                    <label> Kod pocztowy <input type="text"/> </label>
-                    <label> Numer telefonu <input type="tel"/> </label>
+                    <label> Ulica <input onChange={handleChange} value={formData.street} name="street" type="text"/> </label>
+                    {errors.street && <p>{errors.street}</p>}
+                    <label> Miasto <input onChange={handleChange} value={formData.city} name="city" type="text"/> </label>
+                    {errors.city && <p>{errors.city}</p>}
+                    <label> Kod pocztowy <input onChange={handleChange} value={formData.code} name="code" type="text"/> </label>
+                    {errors.code && <p>{errors.code}</p>}
+                    <label> Numer telefonu <input onChange={handleChange} value={formData.phone} name="phone" type="tel"/> </label>
+                    {errors.phone && <p>{errors.phone}</p>}
                 </div>
                 <div>
                     <h3>Termin odbioru:</h3>
-                    <label> Data <input type="date"/> </label>
-                    <label> Godzina <input type="time"/> </label>
-                    <label> Uwagi dla kuriera <textarea/> </label>
+                    <label> Data <input onChange={handleChange} value={formData.date} name="date" type="date"/> </label>
+                    {errors.date && <p>{errors.date}</p>}
+                    <label> Godzina <input onChange={handleChange} value={formData.hour} name="hour" type="time"/> </label>
+                    {errors.date && <p>{errors.date}</p>}
+                    <label> Uwagi dla kuriera <textarea onChange={handleChange} value={formData.desc} name="desc"/> </label>
                 </div>
             </form>
-            {counter>5 ? null : <div>{counter===1 ? null : <button onClick={Minus}>Wstecz</button>}
-                <button onClick={Plus}>{counter===5 ? "Potwierdź" : "Dalej"}</button>
+            {counter > 5 ? null : <div>{counter === 1 ? null : <button onClick={Minus}>Wstecz</button>}
+                <button onClick={four}>{counter === 5 ? "Potwierdź" : "Dalej"}</button>
             </div>}
         </div>
     );
--- src/components/GiveThings/ThingsToGive/Step5.js
+++ src/components/GiveThings/ThingsToGive/Step5.js
@@ -1,24 +1,35 @@
 import React from 'react';
+import icon1 from "../../../assets/Icon-1.svg"
+import icon4 from "../../../assets/Icon-4.svg"
 
 export default function Step5({props}) {
-    const{counter, handleChange, formData, Minus, Plus}=props;
+    const {counter, handleChange, formData, Minus, Plus} = props;
     return (
-        <div>
+        <div className="step1 step5">
             <h1>Podsumowanie Twojej darowizny</h1>
-            <h3>Oddajesz: {formData.gift}</h3>
-            <h5></h5>
-            <h5></h5>
+            <h3>Oddajesz: </h3>
+            <h4><img src={icon1} alt=""/> {formData.bags} worki, {formData.gift}</h4>
+            <h4><img src={icon4} alt=""/>dla lokalizacji: {formData.localization}</h4>
+            <div className="form-step4">
+                <div className="resume">
+                    <h3>Adres odbioru:</h3>
+                    <h4>Ulica {formData.street} </h4>
+                    <h4>Miasto {formData.city} </h4>
+                    <h4>Kod pocztowy {formData.code} </h4>
+                    <h4>Numer telefonu {formData.phone} </h4>
+                </div>
+                <div className="resume">
+                    <h3>Termin odbioru:</h3>
+                    <h4>Data {formData.date}</h4>
+                    <h4>Godzina {formData.hour}</h4>
+                    <h4>Uwagi dla kuriera {formData.desc}</h4>
+                </div>
+            </div>
             <div>
-                <h3>Adres odbioru:</h3>
-
-
-
-
-                <h3>Termin odbioru:</h3>
+                <button onClick={Minus}>Wstecz</button>
+                <button onClick={Plus}>Potwierdzam</button>
             </div>
-             <div> <button onClick={Minus}>Wstecz</button>
-                <button onClick={Plus}>Potwierdź</button>
-            </div>
+
         </div>
     );
 }
